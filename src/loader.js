5a6288ec1d0f338b5ca9f3315f1f025b464eb2a3
SurelyNotChase <chase.lear@outlook.com>
2020-11-22 20:47:27 -0500

    style changes

--- src/loader.js
+++ src/loader.js
@@ -1,8 +1,16 @@
 import * as main from "./main.js";
 window.onload = () => {
-	console.log("window.onload called");
-	// 1 - do preload here - load fonts, images, additional sounds, etc...
+    console.log("window.onload called");
+    // 1 - do preload here - load fonts, images, additional sounds, etc...
+    const frizReg = new FontFace('Friz Quadrata Regular', 'url(../fonts/Friz-Quadrata-Font/Regular.ttf)');
+    frizReg.load().then(function (loadedFont) {
+        document.fonts.add(loadedFont)
+        text.style.fontFamily = '"Friz Quadrata Regular"';
+    }).catch(function (error) {
+        console.log('Failed to load font: ' + error)
+    })
 
-	// 2 - start up app
-	main.init();
+
+    // 2 - start up app
+    main.init();
 }
--- src/main.js
+++ src/main.js
@@ -1,60 +1,69 @@
 import * as lol from './lol.js'
 import * as utils from './utils.js'
+
 function init() {
 
     //event handler for league search button
     document.querySelector('#summonerSearchButton').onclick = () => {
-            lol.listSummonerData();
+        lol.listSummonerData();
     }
 
-    let videoUrls = ['camile','kindred','tristana','xayah','warwick']
+    let videoUrls = ['camile', 'kindred', 'xayah', 'warwick']
 
     //randomize animated background
-    document.querySelector('.fullscreen-bg__video').src = `./media/${videoUrls[utils.getRandomInt(0,videoUrls.length)]}.webm`
+//    let randomInt = utils.getRandomInt(0, videoUrls.length);
+//    if (randomInt == 2) {
+//        document.querySelector('h1').style.color = "black"
+//    } else {
+//        document.querySelector('h1').style.color = "white"
+//    }
+
+    document.querySelector('.fullscreen-bg__video').src = `./media/${videoUrls[randomInt]}.webm`
 }
 
 
-    let cal = new Vue({
-        el: '#calendar',
-        data: {
-            title: "Calendar Events",
-            result: {}
-        },
-        created() {
-            this.search()
-        },
-        methods: {
-            search() {
-                fetch("https://calendarific.com/api/v2/holidays?&api_key=b3de703f8dcbb296bcf442aa7481ea5561f8adb8&country=US&month=2&year=2019")
-                    .then(response => {
-                        if (!response.ok) {
-                            throw Error(`ERROR: ${response.statusText}`);
-                        }
-                        return response.json();
-                    })
-                    .then(json => {
-                        console.log(json.response);
-                        this.result = json.response;
-                        console.log(this.result.holidays.length);
-                    });
-            }, // end search
-            fillHTML() {
-                let holidayArr = this.result.holidays;
-                let tableEle = document.querySelector("#calData");
-                for (let i = 0; i < holidayArr.length; i++) {
-                    let holiEle = document.createElement("tr");
-                    let holiData = document.createElement("td");
-                    holiData.innerHTML = holidayArr[i].name;
-                    holiEle.appendChild(holiData);
-                    holiData.innerHTML = holidayArr[i].description;
-                    holiEle.appendChild(holiData);
-                    tableEle.appendChild(holiEle);
-                }
+let cal = new Vue({
+    el: '#calendar',
+    data: {
+        title: "Calendar Events",
+        result: {}
+    },
+    created() {
+        this.search()
+    },
+    methods: {
+        search() {
+            fetch("https://calendarific.com/api/v2/holidays?&api_key=b3de703f8dcbb296bcf442aa7481ea5561f8adb8&country=US&month=2&year=2019")
+                .then(response => {
+                    if (!response.ok) {
+                        throw Error(`ERROR: ${response.statusText}`);
+                    }
+                    return response.json();
+                })
+                .then(json => {
+                    console.log(json.response);
+                    this.result = json.response;
+                    console.log(this.result.holidays.length);
+                });
+        }, // end search
+        fillHTML() {
+            let holidayArr = this.result.holidays;
+            let tableEle = document.querySelector("#calData");
+            for (let i = 0; i < holidayArr.length; i++) {
+                let holiEle = document.createElement("tr");
+                let holiData = document.createElement("td");
+                holiData.innerHTML = holidayArr[i].name;
+                holiEle.appendChild(holiData);
+                holiData.innerHTML = holidayArr[i].description;
+                holiEle.appendChild(holiData);
+                tableEle.appendChild(holiEle);
             }
-        } // end methods
-    });
+        }
+    } // end methods
+});
 
-    //cal.fillHTML();
+//cal.fillHTML();
 
-export { init }
-
+export {
+    init
+}
