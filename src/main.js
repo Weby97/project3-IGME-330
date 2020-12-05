7c90c3ee7dcf29fe3ca4aceb370450bb343452de
SurelyNotChase <chase.lear@outlook.com>
2020-12-05 01:06:10 -0500

    organization

--- src/main.js
+++ src/main.js
@@ -25,66 +25,54 @@ let app = new Vue({
         getRandomVideoUrl() {
             return `./media/${this.videoNames[utils.getRandomInt(0,this.videoNames.length)]}.webm`
         },
-        searchHover(){
-          searchButton.style.display= none;
-        searchHoverButton.style.display=block;
+        searchHover() {
+            searchButton.style.display = none;
+            searchHoverButton.style.display = block;
         },
         searchClicked() {
-
             lol.listSummonerData(this.summonerInfo.sn, this.summonerInfo.region, summonerDataCallback);
 
-            localStorage.setItem("summonerInfo",JSON.stringify(this.summonerInfo))
-
-            // The Calendar magic happens here
-
-
-
-//            for(let i = 0; i < 3; i++){
-//                let time = utils.getDateFromTimeStamp(this.summonerInfo.masteredChampions[i].lastPlayTime);
-//                cal.listCalendarData(time, calendarDataCallback);
-//            }
-
-            localStorage.setItem("summonerInfo",JSON.stringify(this.summonerInfo.sn));
-           // window.location = 'results.html';
-
-            function calendarDataCallback(e){
-                console.log("calendar data fetched");
-                let calendarData = JSON.parse(e.target.response);
-                console.log(calendarData.response.holidays);
-            }
-            function summonerDataCallback(e) {
-                console.log("summoner data fetched")
-                let summonerData = JSON.parse(e.target.response)
-                console.log(summonerData.id)
-                app.summonerInfo.id = summonerData.id;
-
-
-                lol.listMasteries(app.summonerInfo, masteryCallback)
-
-                function masteryCallback(e) {
-                    console.log('mastery data fetched')
-                    let summonerMasteryData = JSON.parse(e.target.response)
-                    app.summonerInfo.masteredChampions = summonerMasteryData;
-                    console.log(app)
-                    app.dataFinished();
-
-
-                }
-
-            }
-
-
         },
-        dataFinished(){
 
-  console.log(app.summonerInfo.masteredChampions[0])
-
-        }
 
 
     }
 });
 
+
+function calendarDataCallback(e) {
+    console.log("calendar data fetched");
+    let calendarData = JSON.parse(e.target.response);
+    console.log(calendarData.response.holidays);
+}
+
+function summonerDataCallback(e) {
+    console.log("summoner data fetched")
+    let summonerData = JSON.parse(e.target.response)
+    console.log(summonerData.id)
+    app.summonerInfo.id = summonerData.id;
+
+
+    lol.listMasteries(app.summonerInfo, masteryCallback)
+
+    function masteryCallback(e) {
+        console.log('mastery data fetched')
+        let summonerMasteryData = JSON.parse(e.target.response)
+        app.summonerInfo.masteredChampions = summonerMasteryData;
+        console.log(app)
+        dataFinished();
+
+
+    }
+
+}
+
+function dataFinished() {
+            localStorage.setItem("summonerInfo", JSON.stringify(app.summonerInfo))
+            window.location = 'results.html';
+            console.log(app.summonerInfo.masteredChampions[0])
+
+        }
 
 function init() {
     let videoUrls = ['camile', 'kindred', 'xayah', 'warwick']
