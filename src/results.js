3cab9b92ef72a139eca0c42b89aaac83bdbebefd
SurelyNotChase <chase.lear@outlook.com>
2020-12-05 20:20:00 -0500

    new data structure

--- src/results.js
+++ src/results.js
@@ -3,14 +3,45 @@ let app = new Vue({
 
     data: {
         summonerInfo: JSON.parse(localStorage.getItem("summonerInfo")),
-        championName: 'Ahri',
-        championImageUrl: `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${5}_0.jpg`,
-        masteryLevel: 7,
-        masteryImageUrl: 'https://people.rit.edu/cal7114/330/projects/Lear_P3Final/images/m7.png',
-        chestAquired: false,
-        lastPlayDate: "Last played 12/5/2020"
+        threeChampions: [{
+            championName: '',
+            championImageUrl: ``,
+            masteryLevel: 0,
+            masteryImageUrl: ``,
+            chestAquired: false,
+            lastPlayDate: "Last played 12/5/2020"
+        }, {
+            championName: '',
+            championImageUrl: ``,
+            masteryLevel: 0,
+            masteryImageUrl: ``,
+            chestAquired: false,
+            lastPlayDate: "Last played 12/5/2020"
+        }, {
+            championName: '',
+            championImageUrl: ``,
+            masteryLevel: 0,
+            masteryImageUrl: ``,
+            chestAquired: false,
+            lastPlayDate: "Last played 12/5/2020"
+        }],
+
     },
-    created(){
+    created() {
+
+        for (let i = 0; i < 2; i++) {
+            this.threeChampions[i].championName = this.summonerInfo.masteredChampions[i].name,
+            this.threeChampions[i].championImageUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${this.championName}_0.jpg`,
+            this.threeChampions[i].masteryLevel = this.summonerInfo.masteredChampions[i].championLevel,
+            this.threeChampions[i].masteryImageUrl = `https://people.rit.edu/cal7114/330/projects/Lear_P3Final/images/m${this.masteryLevel}.png`,
+            this.threeChampions[i].chestAquired = this.summonerInfo.masteredChampions[i].chestGranted,
+            this.threeChampions[i].lastPlayDate = "Last played 12/5/2020"
+
+
+
+        }
+
+
 
         console.log(this)
     },
--- src/vue-components.js
+++ src/vue-components.js
@@ -1,17 +1,34 @@
 Vue.component('champion-card', {
- props: ['championname','championimageurl','masterylevel','masteryimageurl','lastplayDate','chestaquired'],
+ props: ['champion'],
             template: `
 <div class="championCard">
-        <img v-if='masterylevel>=5' id='masteryIcon' :src="masteryimageurl">
+        <img v-if='champion.masterylevel>=5' id='masteryIcon' :src="champion.masteryimageurl">
         <div class="image-cropper">
-            <img :src="championimageurl" alt="avatar" class="profile-pic">
+            <img :src="champion.championimageurl" alt="avatar" class="profile-pic">
         </div>
 
         <div class='caption'>
-        <div class = "name">{{championname}}</div>
-        <div id = "masteryinfo">{{lastplaydate}}</div>
-        <div v-if='!chestaquired' id = "info1"> <img id='chestIcon' src="images/ChestIconGold.png"> <span>Chest Not Yet Aquired</span></div>
+        <div class = "name">{{champion.championname}}</div>
+        <div id = "masteryinfo">{{champion.lastplaydate}}</div>
+        <div v-if='!champion.chestaquired' id = "info1"> <img id='chestIcon' src="images/ChestIconGold.png"> <span>Chest Not Yet Aquired</span></div>
         </div>
     </div>`
        });
+
+//Vue.component('champion-card', {
+// props: ['championname','championimageurl','masterylevel','masteryimageurl','lastplaydate','chestaquired'],
+//            template: `
+//<div class="championCard">
+//        <img v-if='masterylevel>=5' id='masteryIcon' :src="masteryimageurl">
+//        <div class="image-cropper">
+//            <img :src="championimageurl" alt="avatar" class="profile-pic">
+//        </div>
+//
+//        <div class='caption'>
+//        <div class = "name">{{championname}}</div>
+//        <div id = "masteryinfo">{{lastplaydate}}</div>
+//        <div v-if='!chestaquired' id = "info1"> <img id='chestIcon' src="images/ChestIconGold.png"> <span>Chest Not Yet Aquired</span></div>
+//        </div>
+//    </div>`
+//       });
 
